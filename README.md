c08f32e55c7cb60a3836a7668c1ec3ecf6d08a86
Rafael Rotiroti <rotirotirafa@gmail.com>
2020-04-18 17:10:48 -0300

    configurando README

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 # node-google-spreadsheets
 Script para manipular planilhas do Google
 
+#### Clone este repositório
+``` git@github.com:rotirotirafa/node-google-spreadsheets.git  ```
+
+#### Configuração no Google:
+- Crie uma planilha: [Google-Planilhas] (https://docs.google.com/spreadsheets/) ;
+
+### Crie as credenciais de acesso:
+- Crie um novo projeto: [GitHub](https://console.developers.google.com/) ;
+- Acesse: Contas de serviço -> Criar conta de serviço (coloque permissão de editar);
+- Copie o e-mail gerado ex: nome-escolhido@nome-projeto.iam.gserviceaccount.com;
+- Faça o download da credencial em formato JSON;
+
+### Dê permissão ao e-mail gerado na planilha
+
+### Configuração do projeto
+
+- Acesse com:
+``` cd node-google-spreadsheets ```
+
+- Instale as dependencias necessárias com:
+``` npm install ``` ou ``` yarn ```
+
+- Crie uma pasta chamada 'credentials' dentro da raiz do projeto
+
+- Faça download do JSON gerado e cole dentro da pasta 'credentials'  com o nome de google.json
+
+#### Acesse no navegador e copie o ID inteiro entre '/d/IDAQUI/'
+- Atribua o ID copiado na variável 'sheetId'
 
--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const GoogleSpreadSheet = require('google-spreadsheet');
 const credentials = require('./credentials/google.json');
+const { promisify } = require('util');
 
 const sheetId = '1UvlDOQVhyn875xAYPaEkd9JGHwGJ24iL7kZe0RqL3Bo';
 const sheet = new GoogleSpreadSheet(sheetId);
 
-sheet.useServiceAccountAuth(credentials, err =>{
-    sheet.getInfo((err, info) => {
-        console.log(info);
-    })
-})
+const accessSheet = async() => {
+    try {
+        await promisify(sheet.useServiceAccountAuth)(credentials);
+        const info = await promisify(sheet.getInfo)()
+        const worksheet = info.worksheets[0];
+        //Insert data
+        //await promisify(worksheet.addRow)({marca: 'Ferrari', veiculo: 'Enzo'})
+        
+        //Get data
+        const rows = await promisify(worksheet.getRows)({
+            // offset:2 //usar para paginação
+            //query: 'marca = VW'
+        })
+        console.log(rows)
+        rows.forEach(row => {
+            console.log(row.marca, row.veiculo)
+            //row.del() //enviar marca e veiculo para deletar
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+accessSheet();
